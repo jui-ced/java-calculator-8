62a370d74c5cc271a80976fdda201bf90d59ad85
jui-ced <jiwoo8775@gmail.com>
2025-10-19 14:47:45 +0900

    feat(StringCalculator): 기본 구분자(, :) 파싱 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,6 +4,18 @@ public class StringCalculator {
     public static int sum(String input){
         if(input == null) {
             return 0;
-        }  return 0;
+        }
+        String[] numbers = split(input);
+        return sum(numbers);
+    }
+
+    private static String[] split(String input){
+        return input.split(",|:");
+    }
+
+
+    private static int sum(String[] numbers){
+        int total = 0;
+        return total;
     }
 }
