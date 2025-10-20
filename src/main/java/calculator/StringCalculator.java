afba9c355156374a8c324f8d3d3f2f4d37b4ef30
jui-ced <jiwoo8775@gmail.com>
2025-10-20 12:58:05 +0900

    style(StringCalculator): 코드 들여쓰기 및 주석 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -11,25 +11,34 @@ public class StringCalculator {
         return sum(numbers);
     }
 
+    /*
+     문자열을 구분자 기준으로 분리한다.
+     */
     private static String[] split(String input) {
         if (input.startsWith("//")) {
             int lineIndex = input.indexOf("\\n");
             if (lineIndex == -1) {
                 throw new IllegalArgumentException("[ERROR] 잘못된 입력 형식입니다.");
             }
+
             String delimiter = input.substring(2, lineIndex);
             if (delimiter.isEmpty()) {
                 throw new IllegalArgumentException("[ERROR] 구분자가 비어 있습니다.");
             }
+
             String numbersPart = input.substring(lineIndex + 2);
             if (numbersPart.isEmpty()) {
                 throw new IllegalArgumentException("[ERROR] 숫자 부분이 비어있습니다.");
             }
+            // 커스텀 + 기본 구분자 모두 허용
             return numbersPart.split(Pattern.quote(delimiter) + "|,|:");
         }
         return input.split(",|:");
     }
 
+    /*
+    분리된 문자열 배열을 정수로 변환하여 합산한다.
+     */
     private static int sum(String[] numbers) {
         int total = 0;
 
